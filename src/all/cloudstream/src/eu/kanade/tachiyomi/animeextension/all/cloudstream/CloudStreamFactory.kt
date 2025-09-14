179f525035008080f926a4029c9c9d93830868ed
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-09-14 20:58:40 +0200

    preferences and Plugin class

--- src/all/cloudstream/src/eu/kanade/tachiyomi/animeextension/all/cloudstream/CloudStreamFactory.kt
+++ src/all/cloudstream/src/eu/kanade/tachiyomi/animeextension/all/cloudstream/CloudStreamFactory.kt
@@ -2,38 +2,96 @@ package eu.kanade.tachiyomi.animeextension.all.cloudstream
 
 import android.app.Application
 import android.util.Log
+import androidx.preference.EditTextPreference
+import androidx.preference.MultiSelectListPreference
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.animesource.AnimeSource
 import eu.kanade.tachiyomi.animesource.AnimeSourceFactory
+import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
+import okhttp3.Request
+import okhttp3.Response
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
-class CloudStreamFactory : AnimeSourceFactory { // ConfigurableAnimeSource
-    private val context = Injekt.get<Application>()
-//    private val preferences: SharedPreferences by lazy {
-//        Injekt.get<Application>().getSharedPreferences("source_cloudstream", 0x0000)
-//    }
 
-    override fun createSources(): List<MainApiAdapter> {
+
+class CloudStreamFactory : AnimeSourceFactory {
+    private val context = Injekt.get<Application>()
+
+    override fun createSources(): List<AnimeSource> {
         val apis = PluginLoader.loadAllPlugins(context)
         Log.d("CloudStream", "Loaded ${apis.size}")
-        return apis.map { api -> MainApiAdapter(api) }
+        return apis.map { api -> MainApiAdapter(api) } + Settings()
     }
 }
 
+class Settings() : AnimeHttpSource(), ConfigurableAnimeSource {
+    override val lang: String = "all"
+    override val name: String = "CloudStream Settings"
 
-//    override fun setupPreferenceScreen(screen: PreferenceScreen) {
-//        SwitchPreferenceCompat(screen.context).apply {
-//            key = "TST_KEY"
-//            title = "Test title"
-//            setDefaultValue(true)
-// //            setOnPreferenceChangeListener { _, newValue ->
-// //                preferences.edit().putBoolean(key, newValue as Boolean).commit()
-// //            }
-//            summary = "Test desc"
-//        }.also(screen::addPreference)
-//    }
-//
-//    fun getId(): Long {
-//        return javaClass.name.hashCode().toLong()
-//    }
-//    fun getLang() = "all"
-//    fun getName() = "CloudStream Settings"
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        EditTextPreference(screen.context).apply {
+            key = "REPOS"
+            title = "List of repo URLs"
+            setDefaultValue("")
+            //            setOnPreferenceChangeListener { _, newValue ->
+            //                preferences.edit().putBoolean(key, newValue as Boolean).commit()
+            //            }
+            summary = "Repository URLs (one per line)"
+        }.also(screen::addPreference)
+
+        //TODO input(s) for filtering list below by language/type(movie/tv/anime)
+
+        MultiSelectListPreference(screen.context).apply {
+            key = "EXTENSIONS"
+            title = "Choose extensions to install"
+            entries = arrayOf("A", "B", "C")
+            entryValues = arrayOf("a", "b", "c")
+            setDefaultValue(emptySet<String>())
+        }.also(screen::addPreference)
+    }
+
+    override val baseUrl: String = ""
+    override val supportsLatest: Boolean = false
+    override fun animeDetailsParse(response: Response): SAnime {
+        TODO("Not yet implemented")
+    }
+
+    override fun episodeListParse(response: Response): List<SEpisode> {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesParse(response: Response): AnimesPage {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request {
+        TODO("Not yet implemented")
+    }
+
+    override fun popularAnimeParse(response: Response): AnimesPage {
+        TODO("Not yet implemented")
+    }
+
+    override fun popularAnimeRequest(page: Int): Request {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchAnimeParse(response: Response): AnimesPage {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchAnimeRequest(
+        page: Int,
+        query: String,
+        filters: AnimeFilterList,
+    ): Request {
+        TODO("Not yet implemented")
+    }
+}
+
--- src/all/cloudstream/src/eu/kanade/tachiyomi/animeextension/all/cloudstream/PluginLoader.kt
+++ src/all/cloudstream/src/eu/kanade/tachiyomi/animeextension/all/cloudstream/PluginLoader.kt
@@ -1,10 +1,14 @@
 package eu.kanade.tachiyomi.animeextension.all.cloudstream
 
 import android.app.Application
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import com.lagradost.cloudstream3.APIHolder
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 import dalvik.system.PathClassLoader
 import java.io.File
 import java.io.InputStreamReader
@@ -12,21 +16,29 @@ import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 
 object PluginLoader {
     private const val PLUGIN_FOLDER = "cloudstream"
+    private val handler by lazy { Handler(Looper.getMainLooper()) }
 
     private fun copyIfNeeded(src: File, dest: File) {
         if (!dest.exists() ||
             src.length() != dest.length() ||
             src.lastModified() != dest.lastModified()
         ) {
+            // Make it writable if it exists
+            if (dest.exists() && !dest.canWrite()) {
+                dest.setWritable(true)
+            }
+
             src.copyTo(dest, overwrite = true)
             dest.setLastModified(src.lastModified())
         }
-        // Make sure file is read-only for Android 14+
+        // Make sure file is read-only (for Android 14+ to read dex files)
         dest.setReadOnly()
     }
 
     private fun loadPlugin(context: Application, file: File) {
         try {
+            // Pass extension's classloader as parent (not Aniyomi's) so Cloudstream core classes
+            // (like BasePlugin, MainAPI) that are bundled in this library are available
             val loader = PathClassLoader(file.absolutePath, this::class.java.classLoader)
             loader.getResourceAsStream("manifest.json").use { stream ->
                 if (stream == null) return
@@ -35,7 +47,15 @@ object PluginLoader {
                     val pluginClass = loader.loadClass(manifest.pluginClassName)
                     val pluginInstance = pluginClass.getDeclaredConstructor().newInstance() as BasePlugin
 
-                    pluginInstance.load()
+                    if (pluginInstance is Plugin) {
+                        Log.d("CloudStream","Plugin skipped (Plugin class not yet supported) $file")
+                        handler.post {
+                            Toast.makeText(context, "Plugin ${manifest.name} not yet supported", Toast.LENGTH_SHORT).show()
+                        }
+                        //pluginInstance.load(context) // Not sure what to pass here and how it is used
+                    } else {
+                        pluginInstance.load()
+                    }
                 }
             }
         } catch (e: Throwable) {
@@ -57,7 +77,7 @@ object PluginLoader {
         pluginFiles.forEach { src ->
             val dest = File(internalDir, src.name)
             copyIfNeeded(src, dest)
-            loadPlugin(context, dest)
+            loadPlugin(context,dest)
         }
 
         // Plugins register themselves in APIHolder during load()
