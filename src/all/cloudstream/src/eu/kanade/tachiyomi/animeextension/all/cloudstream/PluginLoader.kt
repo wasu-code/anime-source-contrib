266588f3bf03e3351aef81027d01473ead3a3aae
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-09-18 11:25:38 +0200

    misc: Show toast when installed plugin requires settings (is not supported)

--- src/all/cloudstream/src/eu/kanade/tachiyomi/animeextension/all/cloudstream/PluginLoader.kt
+++ src/all/cloudstream/src/eu/kanade/tachiyomi/animeextension/all/cloudstream/PluginLoader.kt
@@ -32,11 +32,13 @@ object PluginLoader {
                     val pluginInstance = pluginClass.getDeclaredConstructor().newInstance() as BasePlugin
 
                     if (pluginInstance is Plugin) {
-                        Log.d("CloudStream","Plugin skipped (Plugin class not yet supported) $file")
-                        handler.post {
-                            Toast.makeText(context, "Plugin ${manifest.name} not yet supported", Toast.LENGTH_SHORT).show()
+                        Log.d("CloudStream","Class Plugin not yet fully supported: ${manifest.name} ($file)")
+
+                        val hasOpenSettings = pluginInstance.openSettings != null
+                        hasOpenSettings && handler.post {
+                            Toast.makeText(context, "Plugin ${manifest.name} not supported", Toast.LENGTH_SHORT).show()
                         }
-//                        pluginInstance.load(context) // skip openSettings here
+                        pluginInstance.load(context)
                     } else {
                         pluginInstance.load()
                     }
